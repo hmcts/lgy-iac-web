a8b642cf395886e6e542377bd8efd90af2d9450b
Mark Butler <mark.butler@solirius.com>
2022-07-26 17:16:19 +0100

    Fix unused import

--- src/smokeTest/java/uk/gov/hmcts/legacy/web/SampleSmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/legacy/web/SampleSmokeTest.java
@@ -10,7 +10,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;

f97b4a59c4d70cb33fbf8db0d30103075d58b972
Mark Butler <42868152+mark-butler-solirius@users.noreply.github.com>
2022-07-29 19:21:58 +0100

    Add postgres module back after keyvault creation

--- infrastructure/vault.tf
+++ infrastructure/vault.tf
@@ -10,22 +10,3 @@ module "vault" {
   create_managed_identity    = true
   common_tags                = var.common_tags
 }
-
-/*
-# Set by the Jenkins pipeline
-variable "mgmt_subscription_id" {}
-
-provider "azurerm" {
-  alias           = "mgmt"
-  subscription_id = var.mgmt_subscription_id
-  features {}
-}
-
-# for SDS
-data "azurerm_subnet" "jenkins_subnet" {
-  provider             = azurerm.mgmt
-  name                 = "iaas"
-  virtual_network_name = var.env == "sbox" ? "ss-ptlsbox-vnet" : "ss-ptl-vnet"
-  resource_group_name  = var.env == "sbox" ? "ss-ptlsbox-network-rg" : "ss-ptl-network-rg"
-}
-*/

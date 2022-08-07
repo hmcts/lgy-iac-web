a7ddaa17e2c7cfb09fca3981e701d4c4bf11df5f
Mark Butler <mark.butler@solirius.com>
2022-08-07 12:23:08 +0100

    Use jdbcTemplate to test if database table has been created

--- src/main/java/uk/gov/hmcts/legacy/web/controllers/RootController.java
+++ src/main/java/uk/gov/hmcts/legacy/web/controllers/RootController.java
@@ -3,9 +3,15 @@ package uk.gov.hmcts.legacy.web.controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.flywaydb.core.internal.jdbc.JdbcTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
 
 import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
 import static org.springframework.http.ResponseEntity.ok;
@@ -17,6 +23,11 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @RestController
 public class RootController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RootController.class);
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Operation(summary = "Get welcome api",
         description = "This is a welcome endpoint"
     )
@@ -27,6 +38,15 @@ public class RootController {
     @RequestMapping(value = "/", method = GET, produces = TEXT_PLAIN_VALUE)
 
     public ResponseEntity<String> welcome() {
+
+        String sql = "INSERT INTO recipe (id, user_id, name, ingredients, method) VALUES (?, ?, ?, ?, ?)";
+        try {
+            jdbcTemplate.execute(sql, 1, 1, "carbonara", "eggs", "my method");
+        } catch (SQLException e) {
+            LOGGER.error("jdbc insert failure", e.getMessage());
+        }
+        LOGGER.info("jdbc insert successful");
+
         return ok("Welcome to lgy-iac-web dts-legacy application. My favourite legacy app is "
                       + System.getenv("FAVOURITE_FRUIT"));
     }

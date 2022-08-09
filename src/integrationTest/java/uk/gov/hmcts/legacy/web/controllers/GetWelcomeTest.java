fd37263ce03e64b45705052a98335721f2e60667
Mark Butler <mark.butler@solirius.com>
2022-08-09 23:41:36 +0100

    Set ActiveProfiles for integrationTest to disable flyWay

--- src/integrationTest/java/uk/gov/hmcts/legacy/web/controllers/GetWelcomeTest.java
+++ src/integrationTest/java/uk/gov/hmcts/legacy/web/controllers/GetWelcomeTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -12,6 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
+@ActiveProfiles("test")
 class GetWelcomeTest {
 
     @Autowired

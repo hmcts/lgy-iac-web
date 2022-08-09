b5d0c17d07b7ca6e50ea0cddc29014fa78700782
Mark Butler <mark.butler@solirius.com>
2022-08-09 23:46:07 +0100

    Unused imports

--- src/integrationTest/java/uk/gov/hmcts/legacy/web/controllers/GetWelcomeTest.java
+++ src/integrationTest/java/uk/gov/hmcts/legacy/web/controllers/GetWelcomeTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;

b7d6f91d8a998e155d1611ef00938fbad7703fe7
FirdevsKarabagHMCTS <firdevs.karabag@hmcts.net>
2022-08-24 11:56:37 +0100

    No smoke  test results found error fixed

---
+++ src/smokeTest/java/uk/gov/hmcts/legacy/web/SampleSmokeTest.java
@@ -0,0 +1,44 @@
+package uk.gov.hmcts.legacy.web;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static io.restassured.RestAssured.given;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("smoke")
+class SampleSmokeTest {
+    protected static final String CONTENT_TYPE_VALUE = "application/json";
+
+    @Value("${test-url}")
+    private String testUrl;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.baseURI = testUrl;
+        RestAssured.useRelaxedHTTPSValidation();
+    }
+
+    @Test
+    void smokeTest() {
+        Response response = given()
+            .contentType(ContentType.JSON)
+            .when()
+            .get()
+            .then()
+            .extract().response();
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertTrue(response.asString().startsWith("Welcome"));
+    }
+}

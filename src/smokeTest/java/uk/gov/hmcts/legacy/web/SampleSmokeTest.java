a5370d79ee82318c5f8fb9417fb1a4e8d70a8377
FirdevsKarabagHMCTS <firdevs.karabag@hmcts.net>
2022-08-25 17:03:41 +0100

    logging smoke test url

--- src/smokeTest/java/uk/gov/hmcts/legacy/web/SampleSmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/legacy/web/SampleSmokeTest.java
@@ -8,6 +8,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.Is.is;
@@ -17,6 +19,7 @@ import static org.hamcrest.core.Is.is;
 public class SampleSmokeTest {
     //protected static final String CONTENT_TYPE_VALUE = "application/json";
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SampleSmokeTest.class);
     @Value("${test-url}")
     private String testUrl;
 
@@ -27,6 +30,7 @@ public class SampleSmokeTest {
 
     @Test
     public void smokeTest() {
+        LOGGER.info("*****test url***** : " + testUrl) ;
         given()
             .accept(ContentType.HTML)
             .when()

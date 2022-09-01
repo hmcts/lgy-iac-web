fe63d5ccccda4c50e28f9013bcc2e0e165ec41ce
matthewsearle01 <matthewsearle01@gmail.com>
2022-09-01 12:15:40 +0100

    reverting to welcome action test commit

--- src/functionalTest/java/uk/gov/hmcts/legacy/web/SampleFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/legacy/web/SampleFunctionalTest.java
@@ -14,6 +14,7 @@ import static org.hamcrest.core.Is.is;
 
 @RunWith(SpringRunner.class)
 public class SampleFunctionalTest {
+
     protected static final String CONTENT_TYPE_VALUE = "application/json";
 
     @Value("${TEST_URL:http://localhost:8080}")

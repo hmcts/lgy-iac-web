115eb333114ab23d69341a90923b2ed07d853452
matthewsearle01 <matthewsearle01@gmail.com>
2022-09-01 11:06:06 +0100

    reverting to previous commit

--- src/functionalTest/java/uk/gov/hmcts/legacy/web/SampleSeleniumTest.java
+++ src/functionalTest/java/uk/gov/hmcts/legacy/web/SampleSeleniumTest.java
@@ -3,9 +3,6 @@ package uk.gov.hmcts.legacy.web;
 import org.junit.Test;
 import org.apache.log4j.Logger;
 import uk.gov.hmcts.legacy.web.selenium.BaseTest;
-import uk.gov.hmcts.legacy.web.selenium.WelcomeActions_Scenarios;
-
-import static uk.gov.hmcts.legacy.web.selenium.data.WelcomeActions_Pages.page1;
 
 public class SampleSeleniumTest extends BaseTest {
 
@@ -14,10 +11,7 @@ public class SampleSeleniumTest extends BaseTest {
     @Test
     public void WelcomeActionTest() {
         try {
-            logger.info("Test: IAFT1 User Journey");
             init();
-            WelcomeActions_Scenarios.loadScenarios_WelcomeActionSubmitAppeal();
-            WelcomeActions_Scenarios.runTest_WelcomeAction();
         } catch (Exception e) {
             logger.error("SampleSeleniumTest.WelcomeActionTest - ", e);
         }

616eb46113b25f1427faf5b922f5b6801fc3dd17
matthewsearle01 <matthewsearle01@gmail.com>
2022-09-06 10:25:44 +0100

    commenting out SampleFunctionalTest

--- src/functionalTest/java/uk/gov/hmcts/legacy/web/SampleFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/legacy/web/SampleFunctionalTest.java
@@ -12,25 +12,25 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.Is.is;
 
-@RunWith(SpringRunner.class)
-public class SampleFunctionalTest {
-    protected static final String CONTENT_TYPE_VALUE = "application/json";
-
-    @Value("${TEST_URL")
-    private String testUrl;
-
-    @Before
-    public void before() {
-        RestAssured.useRelaxedHTTPSValidation();
-    }
-
-    @Test
-    public void functionalTest() {
-        given()
-            .accept(ContentType.HTML)
-            .when()
-            .get(testUrl + "/IACFees")
-            .then()
-            .statusCode(200);
-    }
+//@RunWith(SpringRunner.class)
+//public class SampleFunctionalTest {
+//    protected static final String CONTENT_TYPE_VALUE = "application/json";
+//
+//    @Value("${TEST_URL")
+//    private String testUrl;
+//
+//    @Before
+//    public void before() {
+//        RestAssured.useRelaxedHTTPSValidation();
+//    }
+//
+//    @Test
+//    public void functionalTest() {
+//        given()
+//            .accept(ContentType.HTML)
+//            .when()
+//            .get(testUrl + "/IACFees")
+//            .then()
+//            .statusCode(200);
+//    }
 }

fbd49d5d58c7011385191ebe10820def0b76ebec
Mark Butler <mark.butler@solirius.com>
2022-09-15 11:55:27 +0100

    Try exporting mounted secrets in startup shell script

--- deploy/start_tomcat.sh
+++ deploy/start_tomcat.sh
@@ -1,10 +1,19 @@
 # Start process in a subshell
 echo "In start_tomcat.sh"
 echo "UserId is <" + `id`+ ">"
+echo "ls started <" + date + ">"
 find /opt/tomcat -exec ls -l {} \;
+echo "ls finished <" + date + ">"
+
+# Test exporting the mounted secrets
+export SECRET_PATH=/mnt/secrets/lgy-iac/
+export POSTGRES_DATABASE=`cat $SECRET_PATH/POSTGRES_DATABASE`
+export POSTGRES_HOST=`cat $SECRET_PATH/POSTGRES_HOST`
+export POSTGRES_PASSWORD=`cat $SECRET_PATH/POSTGRES_PASSWORD`
+export POSTGRES_USER=`cat $SECRET_PATH/POSTGRES_USER`
+
 (/busybox/sh /opt/tomcat/bin/catalina.sh run)&
 PID=$!
-# Do other stuff
-echo "Hello world!"
+
 # Wait for server to terminate
 wait $PID

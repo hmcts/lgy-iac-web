217292cbf9091a0d99ea0cf217bb3d87113c3d3e
Mark Butler <mark.butler@solirius.com>
2022-09-15 16:43:06 +0100

    Move export method in startup_tomcat to top of script

--- deploy/start_tomcat.sh
+++ deploy/start_tomcat.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Start process in a subshell
-echo "In start_tomcat.sh"
-echo "UserId is <" + `id`+ ">"
+export_mounted_keyvault_values () {
+  for file in $SECRETS_PATH/*
+  do
+    export `basename $file`=`cat $file`
+  done
+}
 
 # Exporting the Azure Keyvault secrets mounted by the java chart before invoking tomcat
 export SECRETS_PATH=/mnt/secrets/lgy-iac/
@@ -13,10 +16,3 @@ PID=$!
 
 # Wait for server to terminate
 wait $PID
-
-export_mounted_keyvault_values() {
-  for file in $SECRETS_PATH/*
-  do
-    export `basename $file`=`cat $file`
-  done
-}
